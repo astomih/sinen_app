c97b247214067597f6ddb59149f38ac644dd16ff
astomih <asamin1090@gmail.com>
2022-02-07 07:10:52 +0000

    Deploying to gh-pages from @ astomih/utilis@e6950cea18bd2c705cfd368977297ee38a7dd785 🚀

---
+++ CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/Actor/Camera.cpp.o"
+  "CMakeFiles/main.dir/src/Actor/Camera.cpp.o.d"
+  "CMakeFiles/main.dir/src/Actor/enemy.cpp.o"
+  "CMakeFiles/main.dir/src/Actor/enemy.cpp.o.d"
+  "CMakeFiles/main.dir/src/Actor/player.cpp.o"
+  "CMakeFiles/main.dir/src/Actor/player.cpp.o.d"
+  "CMakeFiles/main.dir/src/Scene/Stage.cpp.o"
+  "CMakeFiles/main.dir/src/Scene/Stage.cpp.o.d"
+  "CMakeFiles/main.dir/src/Scene/Title.cpp.o"
+  "CMakeFiles/main.dir/src/Scene/Title.cpp.o.d"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "CMakeFiles/main.dir/src/model/model.cpp.o"
+  "CMakeFiles/main.dir/src/model/model.cpp.o.d"
+  "main.html"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ assets/Shader/GL/alpha.frag
@@ -0,0 +1,11 @@
+#version 330 core
+precision mediump float;
+in vec2 outUV;
+in vec4 outRgba;
+out vec4 outColor;
+uniform sampler2D diffuseMap;
+void main()
+{
+  vec4 color = outRgba*texture(diffuseMap, outUV);
+  outColor = color;
+}
