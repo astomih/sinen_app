b55646923adcbf7758658e77c847e7a48df6a546
Astomih <asamin1090@gmail.com>
2022-02-27 21:25:12 +0900

    Update

--- data/script/scene01.lua
+++ data/script/scene01.lua
@@ -1,21 +1,39 @@
 local utility = require("utility")
 
-hello_texture = {}
-function setup()
-    print("scene01.lua: setup()")
-    hello_texture = create_texture()
-    texture_fill_color(hello_texture, color.new(1, 1, 0, 1), "red")
+local hello_drawer = {}
+local hello_drawer2 = {}
+local hello_drawer3 = {}
+local hello_texture = {}
+local hello_texture2 = {}
 
-    print("scene01.lua: end setup()")
+function setup()
+    hello_texture = texture()
+    hello_texture:fill_color(color(1, 1, 1, 1))
+    hello_texture2 = texture()
+    hello_texture2:fill_color(color(1, 1, 1, 1))
+
+    hello_drawer = draw2d(hello_texture)
+    hello_drawer.position = vector2(0, 0)
+    hello_drawer.scale = vector2(100, 100)
+    hello_drawer.rotation = 0
+
+    hello_drawer2 = draw2d(hello_texture)
+    hello_drawer2.position = vector2(0, 0)
+    hello_drawer2.scale = vector2(100, 100)
+    hello_drawer2.rotation = 0
+
+    hello_drawer3 = draw3d(hello_texture)
+    hello_drawer3.scale = vector3(1, 1, 1)
+    hello_drawer3.rotation = vector3(0, 0, 0)
 end
 
 function update()
+    hello_drawer:draw()
+    hello_drawer.rotation = hello_drawer.rotation + 2 * delta_time
+    hello_drawer2:draw()
+    hello_drawer2.rotation = hello_drawer2.rotation + 5 * delta_time
 
-    hello_drawer = draw2d_object.new()
-    hello_drawer.texture = hello_texture
-    hello_drawer.position = vector2.new(100, 0)
-    hello_drawer.scale = vector2.new(200, 200)
-    hello_drawer.rotation = 0
-    draw2d.rect(hello_drawer)
-    print("scene01.lua: update()")
+    hello_drawer3:draw()
+    hello_drawer3.rotation.x = hello_drawer2.rotation + 5 * delta_time
+
 end
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 #include <sinen_lua.hpp>
 
 int main(int argc, char *argv[]) {
-  if (!nen::get_manager().initialize())
+  if (!nen::initialize())
     return -1;
-  nen::load_modules(nen::get_manager().get_script_system().get_sol_state());
-  nen::get_manager().launch();
+  nen::load_modules(nen::get_script_system().get_sol_state());
+  nen::launch();
   return 0;
 }
