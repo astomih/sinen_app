6de0d464b455ff42fb5ae97514013a1fb5c35146
Astomih <asamin1090@gmail.com>
2022-03-04 21:59:33 +0900

    Update

--- data/script/main.lua
+++ data/script/main.lua
@@ -5,11 +5,11 @@ local hello_drawer = {}
 function setup()
     hello_texture = texture()
     hello_drawer = draw2d(hello_texture)
-    hello_drawer.scale = vector2(1, 1)
     hello_font = font()
     hello_font:load(DEFAULT_FONT, 128)
-    hello_font:render_text(hello_texture, "Hello World!", color(1, 1, 1, 1), 2,
-                           color(1, 1, 1, 1))
+    hello_font:render_text(hello_texture, "Hello Sinen World!",
+                           color(1, 1, 1, 1), 2, color(1, 1, 1, 1))
+    hello_drawer.scale = hello_texture:size()
 
 end
 
